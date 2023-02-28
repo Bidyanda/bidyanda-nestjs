93f220ec5249e4a7b2a5cdae8f4fa57ce5b4f32d
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 01:24:55 +0530

    test6

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Bidyanda!, This is github webhook ready I Love U Reji Devi Kambam Nungshibi nangbune eina ngaorise';
+    return 'Hello Bidyanda!, This is github webhook ready I Love U Reji Devi Kambam Nungshibi nangbune eina ngaorise nangdi eigine';
   }
 }

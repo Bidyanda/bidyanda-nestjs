82fd84edf0d06706d84d922bf418e55730242f9e
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 00:55:15 +0530

    test2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,7 +13,7 @@ export class AppController {
   @Post('/webhook')
   webhook(@Body() body:any) {
     return new Promise<string>((resolve, reject) => {
-        exec('sh /var/www/bidyanda/html/bidyanda/gitpull.sh', (error, stdout, stderr) => {
+        exec('/var/www/bidyanda/html/bidyanda/gitpull.sh', (error, stdout, stderr) => {
           if (error) {
             reject(error);
           } else {

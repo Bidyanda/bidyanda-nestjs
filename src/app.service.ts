48e54ce7ae9b71cb446692832c9df01281235cbb
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 00:51:01 +0530

    simple change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Bidyanda!, This is github webhook ready I Love U Reji';
+    return 'Hello Bidyanda!, This is github webhook ready I Love U Reji Kambam';
   }
 }

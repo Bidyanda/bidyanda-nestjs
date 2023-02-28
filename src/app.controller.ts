41d4b2d6630c4ad915edb96ad2a10c1e51b0a419
Bidyanda <bidyandaningthoujam@gmail.com>
2023-02-28 19:14:56 +0000

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { exec } from 'child_process';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/webhook')
+  webhook(@Body() body:any) {
+    return new Promise<string>((resolve, reject) => {
+        exec('./../gitpull.sh', (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(stdout);
+          }
+        });
+      });
   }
 }

4bc7d62b47cf161d72ef18ac9a4559697becf5e9
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 09:41:30 +0530

    deployment test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Bidyanda!, This is github webhook ready I Love U Reji Devi Kambam Nungshibi nangbune eina ngaorise nangdi eigine';
+    return 'Hello Bidyanda!, Testing for deploy app on node js';
   }
 }

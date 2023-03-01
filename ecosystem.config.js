b702f9c7125df664bfaa39fef943a1b0fe85eb0e
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 12:30:18 +0530

    commit 7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,34 +1,26 @@
 module.exports = {
   apps: [
     {
-      instances: -1,
-      exec_mode: "cluster",
-    },
-    {
+      name: "bidyanda",
+      script: "dist/main.js",
+      instances: "max",
+      autorestart: true,
+      watch: false,
       env: {
-        PORT: 3000,
-        NODE_ENV: "production",
-      },
-      env_staging: {
-        PORT: 3000,
-        NODE_ENV: "staging",
-      },
-      
-      env_production: {
-        PORT: 3000,
         NODE_ENV: "production",
       },
     },
   ],
   deploy: {
-      "production" : {
-        "key"  : "/var/www/sshkey.pem", // path to the public key to authenticate
-        "user" : "root",              // user used to authenticate
-        "host" : "143.110.240.100",      // where to connect
-        "ref"  : "origin/master",
-        "repo" : "git@github.com:Bidyanda/bidyanda-nestjs.git",
-        "path" : "/var/www/bidyanda/html/bidyanda",
-        "post-deploy" : "pm2 startOrRestart ecosystem.config.js --env production"
-      },
+    production: {
+      key: "/var/www/sshkey.pem", // path to the public key to authenticate
+      user: "root", // user used to authenticate
+      host: "143.110.240.100", // where to connect
+      ref: "origin/master",
+      repo: "git@github.com:username/my-nest-app.git",
+      path: "/var/www/bidyanda/html/bidyanda",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
   },
 };

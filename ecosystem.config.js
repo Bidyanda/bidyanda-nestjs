d62187cfccd0d5c4aea74f1aaf2c0b8ac01b1797
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-01 14:24:51 +0530

    test2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,32 @@
+// module.exports = {
+//   apps : [{
+//     name   : "bidyanda",
+//     script : "dist/main.js"
+//   }]
+// }
 module.exports = {
-  apps : [{
-    name   : "bidyanda",
-    script : "dist/main.js"
-  }]
-}
+  apps: [
+    // First application
+    {
+      name: 'bidyanda',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      error_file: 'err.log',
+      out_file: 'bidyanda.log',
+      merge_logs: false,
+      env: {
+        NODE_ENV: 'development',
+        PORT:3000
+      },
+      env_staging: {
+        NODE_ENV: 'staging',
+        PORT:3000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT:3000
+      },
+    },
+  ],
+};

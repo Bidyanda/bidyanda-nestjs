2d3f6b31c329341edef5fa543a8129211c637877
Bidyanda <bidyandaningthoujam@gmail.com>
2023-03-01 08:35:18 +0000

    updaste

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,6 @@
 module.exports = {
-  apps: [
-    {
-      name: "bidyanda",
-      script: "dist/main.js",
-      instances: "max",
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
-  deploy: {
-    production: {
-      key: "/var/www/sshkey.pem", // path to the public key to authenticate
-      user: "root", // user used to authenticate
-      host: "143.110.240.100", // where to connect
-      ref: "origin/master",
-      repo: "git@github.com:username/my-nest-app.git",
-      path: "/var/www/bidyanda/html/bidyanda",
-      "post-deploy":
-        "pm2 startOrRestart ecosystem.config.js --env production",
-    },
-  },
-};
+  apps : [{
+    name   : "bidyanda",
+    script : "dist/main.js"
+  }]
+}

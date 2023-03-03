08d4a38b2f134e53fd82a810157d868850e8a8b6
Bidyanda <53209757+Bidyanda@users.noreply.github.com>
2023-03-03 11:48:16 +0530

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Bidyanda NestJs Project... Server Test----->>>>>><<<<<<<<--------';
+    return 'Welcome to Bidyanda NestJs Project... Server Test----->>>>>>OK<<<<<<<<--------';
   }
 }
